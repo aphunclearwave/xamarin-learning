22670739bafe7aeb161a27783268669ebed874bf
Adan Phun <aphun@clearwaveinc.com>
2020-03-26 09:31:21 -0400

    Initial project setup with two view models for learning

---
+++ Xamarin101/Xamarin101/TapMeButtonPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin101
+{
+    public partial class TapMeButtonPage : ContentPage
+    {
+        public TapMeButtonPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Xamarin101/Xamarin101/TapMeButtonViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin101
+{
+    public class TapMeButtonViewModel : INotifyPropertyChanged
+    {
+        string _buttonText;
+        public string ButtonText
+        {
+            private set
+            {
+                _buttonText = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(ButtonText)));
+                }
+            }
+            get
+            {
+                return _buttonText;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand TapMeCommand { get; private set; }
+
+
+        public TapMeButtonViewModel()
+        {
+            ButtonText = "Tap me!!";
+            TapMeCommand = new Command(OnTapMeTapped);
+        }
+
+        public void OnTapMeTapped()
+        {
+            ButtonText = "You Tapped Me!";
+        }
+    }
+}
